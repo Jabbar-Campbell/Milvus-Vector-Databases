2d2adffdeee3244d406d269ea91b693f6d812bb8
Jabbar Campbell <Jabbar_Campbell@hotmail.com>
2024-07-13 15:26:16 -0400

    model has been loaded need to preprocess

--- ALS DISEASE PROJECT.py
+++ ALS DISEASE PROJECT.py
@@ -1,17 +1,19 @@
-# In this Project the goal is  create a data base of images and then search thru those images using a query image using MILVUS
+# In this Project the goal is  create a database of images and then search thru those images using a query image using MILVUS
 # Milvus is a non structured database unlike sql and uses vectors in n Dimensional space as opposed to searching thru tables
 
 
 # Since we are on a windows pc an milvus runs in a linux enviroment we'll first need to download and install Docker. This will allow
-# us to run separate environments as containers. My machine need the Virtulization to be enabled in the BIOS confif file in order for 
-# Docker to work. Once this was done I could make use of the stand alone Milvus installation. 
-# wget is a linux command so I had to install that first and add it my PATH variable file so I could run wget from any directory
+# us to run separate environments as containers. My machine needed   Virtulization to be enabled in the BIOS config file in order for 
+# Docker to work. Once this was done I could make use of the stand alone Milvus installation. (theres one for cloud infrastructure as well)
+
+# wget is a common linux command for downloading material so I had to install that first.  By  adding it my PATH variable file  I could run 
+# wget from any directory
+
 # Next I downloaded the milvus-standalone-docker-compose.yml from the Milvus website and saved it as docker-compose.yml. I changed
 # directories to something I could work out of and typed the following.....
-#  "wget https://github.com/milvus-io/milvus/releases/download/v2.4.0/milvus-standalone-docker-compose.yml -O docker-compose.yml"
-
-# In the directory that holds docker-compose.yml, 
-# cd "C:\Users\jabba\OneDrive\Desktop\Sandbox\Milvus"
+# "wget https://github.com/milvus-io/milvus/releases/download/v2.4.0/milvus-standalone-docker-compose.yml -O docker-compose.yml" 
+# In the directory that holds the docker-compose.yml file  
+# for me this was "C:\Users\jabba\OneDrive\Desktop\Sandbox\Milvus"
 
 # start Milvus by running: 
 # "sudo docker compose up -d"
@@ -24,20 +26,26 @@
 # sudo docker compose down
 
 # With Milvus running in a Docker Container I then created an python environment in VSCODE that holds all the pacakges required for this
-# project. Once created a ran the activate file located within to turn it onOnce up and running I then instructed my python interpretor 
-# to use this newly activated envirnoment. we now have TWO environoment. One is Docker that is managing a MILVUS database. The other 
-# is a Python Environment that will connect to it. Your VScode terminal command line should say  "(milvus_env) PS C:\your\directory"
-# our Python env is mostly empty except for the basic python packages we'll need to install things like pymilvus so that we can connect 
+# project. Once created I ran the activate file located within to turn it on. In my case it was located  at 
+# C:\Users\jabba\milvus_env\Scripts\Activate.ps1
+# Once up and running I then made sure my python interpretor was using this newly activated envirnoment. (reflected in the lower right
+# of the vscode editor)
+ 
+# we now have TWO "environments"!!. One is Docker that is managing a MILVUS database. The other is a Python Environment that will connect to it. 
+# Your VScode terminal command line should say  "(milvus_env) PS C:\your\home\directory"
+# Since our Python env is mostly empty except for the basic python packages we'll need to install things like pymilvus so that we can connect 
 # to milvus. Vscode also will then know what we're talking about when we start using commands from this package. At the Terminal of 
 # our activated environment type ...... 
-# $ "pip3 install pymilvus protobuf grpcio-tools jupyterlab"
-# if your like me you may  have "pip3 ssl" issues  where pip cant find find ssl certificates. if so use...
+# $ "pip3 install pymilvus protobuf grpcio-tools jupyterlab torchvision"
+
+# if you're like me you may  have "pip3 ssl" issues  where pip cant find find ssl certificates. if so use...
 # $ "pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pymilvus protobuf grpcio-tools jupyterlab"
 
 
 
 # With our Python env populated with the right packages and a Docker container running Milvus
 # Lets connecting to our Milvus Database
+
 import pymilvus
 
 
@@ -49,6 +57,7 @@ from pymilvus import connections, utility, Collection, CollectionSchema, FieldSc
 connections.connect(host = '127.0.0.1',port = 19530)
 
 
+# We'll also need some images and the path to them. OS is a great package for making path variables
 
 ######################################################### LOCATE IMAGE PATHS  ########################################################D
 ###########################################################################################################################################    
@@ -67,26 +76,60 @@ for i in Files:
 
 
 
+# now that we have some test images milvus only stores vectorized information. In order
+# so our goal should be to vectorize these images.  This is done with an embedding process
+# there are many algorithems that do this. Here package management can be an issue since we'll 
+# have to pip install a suitable version  of which there are many
+# for a comphrensive list see https://pypi.org/search/?q=torchvision
+# to start lets make use of the ones readily available from pytorch/vision repo callend "resnet18"
+# we can experiment with others later. Since we dont have pytorch in our envirnoment we need to install it
 
-
-
-
+# remembering our SSL issues type the following......
+# "pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org torch==1.3.1+cpu torchvision"
 
 #random.shuffle(paths)
 
 
-# Load the embedding model from the tensorflow hub with the last layer removed
+
+
+# Here we load an embedding model.  From the pytorch/vision repo a Convolutional Neural Network called resnet 18. This model
+# has been trained on many images (probably not pathology iamges). If you have a basic understanding of neural 
+# networks its no surprise that Convolutional Networks are best for image processing and that the weights
+# between nodes must be optimized using test images and gradient descent in a process call back propagation
+# setting the pretrained argumnet to true will make use of those weights. Inspecting the archithecture of this
+# network we can see the number of nodes in each layer. The algorithem used in back propogation, pooling layer
+# settings and more for an indepth review watch....
+# https://www.youtube.com/watch?v=nc7FzLiB_AY&t=103s
+
+# The first argument is the repo name, the second argument is the model name
+# The third argument is an optional parameter to specify the model version
+
 import torch
-model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
+from torchvision import transforms
+
+
+model = torch.hub.load('pytorch/vision', 'resnet18', pretrained=True)
 model.eval()
 
-# Preprocessing for images
+
+# well need to string some preprocessing procedures together to perform on each image
+# Tranform.compose is a nice way to accomplish this and save that in something we'll call
+# preprocess
+
 preprocess = transforms.Compose([
     transforms.Resize(299),
     transforms.CenterCrop(299),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
+
+
+# lets test this out and inspect our first image
+# comparing them side by side. Transform expects an img so we'll need to convert
+#
+
+preprocess(file_paths[0])
+
 
 
 # Function to create embeddings from the model
@@ -109,4 +152,4 @@ print(len(emb))
 
 # This gives us a nice write up on the math under the hood 
 
-https://towardsdatascience.com/deep-dive-into-vector-databases-by-hand-e9ab71f54f80
+# https://towardsdatascience.com/deep-dive-into-vector-databases-by-hand-e9ab71f54f80
